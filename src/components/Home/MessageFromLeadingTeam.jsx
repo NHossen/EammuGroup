5d223bac337b6fc1e7958af6c92245a334d69d4b
naeem <naeemhossen2509@gmail.com>
2025-06-30 19:38:19 +0400

    team profile updated

--- src/components/Home/MessageFromLeadingTeam.jsx
+++ src/components/Home/MessageFromLeadingTeam.jsx
@@ -6,7 +6,7 @@ const MessageFromLeadingTeam = () => {
       name: "Naeem Hossen",
       role: "Global Visa Expert & Air Ticket Specialist",
       image:
-        "https://scontent.ffjr1-6.fna.fbcdn.net/v/t39.30808-6/500787989_2555738771437282_8352184388819456690_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=127cfc&_nc_ohc=e5PmM8izgecQ7kNvwHlsDY8&_nc_oc=AdkZpqMzYcjHiC_Mxkh-jpgWWzR4OFy0n5LPr-pMcaT0_hoMTFQSpt8jd6FVVGO9PeM&_nc_zt=23&_nc_ht=scontent.ffjr1-6.fna&_nc_gid=yx9wzBpJKEWq7j3ZmvhnTQ&oh=00_AfNrA-dGpLz3xDCddm15M3_O_RRSi7KOyeqIxWdD_AccLg&oe=68609606",
+        "https://i.ibb.co/hxKph5xG/513826752-2571065443237948-8433664019789292699-n.jpg",
       description:
         "Naeem is an experienced global visa advisor specializing in air ticketing and destination consulting. He leads Eammu's vision for global travel and immigration strategy.",
     },
@@ -14,7 +14,7 @@ const MessageFromLeadingTeam = () => {
       name: "Sana Ullah Thasin",
       role: "Student Visa Expert & Customer Service Manager",
       image:
-        "https://scontent.ffjr1-4.fna.fbcdn.net/v/t39.30808-6/508348597_122137393082750708_6187212370402935117_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=833d8c&_nc_ohc=BDjR_lOtj9gQ7kNvwHQF4Yt&_nc_oc=Adm66SurfI4Mir97y97n_OD15MAkQ2dm-yXyCafJl1GEwuvnk7XPm9V5ZBOEgNVhdO4&_nc_zt=23&_nc_ht=scontent.ffjr1-4.fna&_nc_gid=tcfl6hCfhDsoY5aqhBOq9Q&oh=00_AfPn-HfCA7FehfjVYjoqDp4UlLDU9NWFZYxM3iCM0Kvnsw&oe=68606851",
+        "https://i.ibb.co/nsbGwJ4K/497478223-2378373822548199-2151584947092917211-n.jpg",
       description:
         "Sana specializes in student visa guidance for the USA, UK, Canada, and other countries. She manages customer service, providing personalized support to every student.",
     },
@@ -22,7 +22,7 @@ const MessageFromLeadingTeam = () => {
       name: "Wahid Ahamed Emon",
       role: "Customer Service Executive & Tour Expert",
       image:
-        "https://scontent.ffjr1-6.fna.fbcdn.net/v/t39.30808-6/464369930_532778909362696_6020132385522832185_n.jpg?stp=cp6_dst-jpg_tt6&_nc_cat=102&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=7CU7WCk-RmMQ7kNvwGIPh2D&_nc_oc=AdnwhbdNQTNnwt7yHjgk7gEi_0sunL9v4X45X4vwa_SEVG3huTvDWYekQEdL07AssHY&_nc_zt=23&_nc_ht=scontent.ffjr1-6.fna&_nc_gid=HsRHDPIsb5qCX2riph0Ysg&oh=00_AfN9BccNojZJK5X_1uWr5p3g1_xSdKqPxhazW3z32qPTFg&oe=6860700A",
+        "https://i.ibb.co/PGX6RnVb/464369930-532778909362696-6020132385522832185-n.jpg",
       description:
         "Emon provides excellent customer support and crafts unforgettable travel packages as our holiday tour expert.",
     },
