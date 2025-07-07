b58290137e0d933a7ed9615fdc938b4c7fae08eb
naeem <naeemhossen2509@gmail.com>
2025-07-07 23:40:33 +0400

    careers url updated

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -53,7 +53,7 @@ const Footer = () => {
                     <Link className='mb-[6px]' to='/eammunewsfeeds'><a className="link link-hover hover:text-[#005a31]">Eammu Newsfeeds</a></Link>
                     <Link className='mb-[6px]' to='/about'><a className="link link-hover hover:text-[#005a31]">Eammu Recent Projects</a></Link>
                     <Link className='mb-[6px]' to='/messagefromleadingteam'><a className="link link-hover hover:text-[#005a31]">Our Leading Team</a></Link>
-                    <Link className='mb-[6px]' to='/eammucareers'><a className="link link-hover hover:text-[#005a31]">Careers</a></Link>
+                    <Link className='mb-[6px]' to='/careers'><a className="link link-hover hover:text-[#005a31]">Careers</a></Link>
                     <Link className='mb-[6px]' to='/eammuterms'><a className="link link-hover hover:text-[#005a31]">Terms of use & Privacy policy</a></Link>
                     <Link className='mb-[6px]' to='/whychoiceeammu'><a className="link link-hover hover:text-[#005a31]">Why Choose Us</a></Link>
                 </nav>
