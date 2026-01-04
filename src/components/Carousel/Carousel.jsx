6beddc5eb21821944595d021ff0150d695d83ccc
NHossen <naeemhossen2509@gmail.com>
2026-01-04 13:14:39 +0400

    photo updated

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -119,14 +119,14 @@ const Carousel = () => {
         {/* Slide 2 */}
         <div id="slide2" className="carousel-item w-full h-full snap-center relative flex-shrink-0">
           <img
-            src="https://www.universityliving.com/blog/wp-content/uploads/2022/10/Banner-Image-1.webp"
-            className="object-cover w-full h-full opacity-60"
+            src="https://ben.edu/wp-content/uploads/2023/08/Ben-Headers-7.png.webp"
+            className="object-cover w-full h-full"
             alt="Why Choose Eammu - Trusted Immigration Partner"
           />
-           <div className="absolute inset-0 bg-black/30 flex flex-col items-center justify-center text-white text-center px-6">
+           <div className="absolute bg-black/30 inset-0 flex flex-col items-center justify-center text-white text-center px-6">
             <div>
-              <h1 className="text-5xl font-bold mb-4 text-[#005a31]">Welcome to Target IELTS & Immigration Center - TIIC</h1>
-              <p className="text-lg font-bold mb-6">TIIC is recognized as the best language academy for IELTS, PTE, and Spoken English.</p>
+              <h1 className="text-4xl font-bold mb-4 ">Target IELTS & Immigration Center - TIIC</h1>
+              <p className="text-lg font-bold mb-6 ]">TIIC is recognized as the best language academy for IELTS, PTE, and Spoken English.</p>
               <Link 
                 to="/target-ielts-and-immigration-center" 
                 className="btn bg-white text-[#005a31] hover:bg-[#005a31] hover:text-white border border-[#005a31]"
@@ -146,7 +146,7 @@ const Carousel = () => {
         {/* Slide 3 */}
         <div id="slide3" className="carousel-item w-full h-full snap-center relative flex-shrink-0">
           <img
-            src="https://cdn.ama.ab.ca/amatravelinternal/articles/images/double-decker-london.jpg"
+            src="/public/travel.jpg"
             className="object-cover w-full h-full"
             alt="Global Visa Destinations - Eammu Services"
           />
@@ -171,22 +171,19 @@ const Carousel = () => {
         {/* Slide 4 */}
         <div id="slide4" className="carousel-item w-full h-full snap-center relative flex-shrink-0">
           <img
-            src="https://images.unsplash.com/photo-1747276737245-b10e95175577?q=80&w=1172&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            src="/public/eammuicon.jpg"
             className="object-cover w-full h-full"
             alt="Client Testimonials - Real Success Stories"
           />
-          <div className="absolute inset-0 bg-[#005a31]/40 flex items-center justify-center text-white px-6 text-center">
+          <div className="absolute inset-0 flex items-center justify-center text-white px-6 text-center">
             <div>
-              <h2 className="text-4xl font-bold mb-3">Real Stories. Real Success.</h2>
-              <ul className="text-lg space-y-2">
-                <li className='text-lg font-bold'>🎓 Students Visa Approved</li>
-                <li className='text-lg font-bold'>👷 Workers Departing to Europe & UAE</li>
-                <li className='text-lg font-bold'>✈️ Group Tour to Thailand | Photos & Memories</li>
-                <li className='text-lg font-bold'>📸 See Our Client Testimonials & Video Stories</li>
-              </ul>
+              
+             <div className='mt-48'>
               <Link to="/testimonials" className="btn bg-white text-[#005a31] hover:bg-[#005a31] hover:text-white border border-[#005a31] mt-4">
                 Customer Review
               </Link>
+             </div>
+              
             </div>
           </div>
           <div className="absolute left-5 right-5 top-1/2 flex justify-between transform -translate-y-1/2">
